4c98d1beb31f8e758410964f055a6ffd1d657278
Habibur Rahman Ove <anon.haabib@gmail.com>
2023-09-28 16:46:18 +0600

    odd numbers adding

--- array-elements-adding.js
+++ array-elements-adding.js
@@ -1,5 +1,7 @@
-// adding the even numbers of an array
+const arr = [45, 65, 132, 16, 5, 41, 0, 41, 65, 445, 32, 13, 450, 1, 32, 13, 245]
+// const arr = [1, 2, 3, 4]
 
+// adding the even numbers of an array
 function evenAdding (array) {
     let sum = 0;
     for(let i = 0; i <= array.length; i++){
@@ -8,6 +10,16 @@ function evenAdding (array) {
         }
     } return sum;
 }
-
-const arr = [45, 65,132, 16, 5, 41, 0, 41, 65, 445]
 console.log(evenAdding(arr));
+
+
+// adding the odd numbers of an array
+function oddAdding (array){
+    let sum = 0;
+    for (let i = 0; i <= array.length; i++){
+        if (array[i] % 2 === 1){
+            sum = sum + array[i];
+        }
+    } return sum;
+}
+console.log(oddAdding(arr));
