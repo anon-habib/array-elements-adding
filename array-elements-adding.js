8d4252dcaf54b1b24653aed4fba501731df810e7
Habibur Rahman Ove <anon.haabib@gmail.com>
2023-09-28 17:39:49 +0600

    final

--- array-elements-adding.js
+++ array-elements-adding.js
@@ -4,7 +4,7 @@ const arr = [45, 65, 132, 16, 5, 41, 0, 41, 65, 445, 32, 13, 450, 1, 32, 13, 245
 // adding the even numbers of an array
 function evenAdding (array) {
     let sum = 0;
-    for(let i = 0; i <= array.length; i++){
+    for(let i = 0; i < array.length; i++){
         if (array[i] % 2 === 0){
             sum = sum + array[i];
         }
@@ -16,8 +16,8 @@ console.log(evenAdding(arr));
 // adding the odd numbers of an array
 function oddAdding (array){
     let sum = 0;
-    for (let i = 0; i <= array.length; i++){
-        if (array[i] % 2 === 1){
+    for (let i = 0; i < array.length; i++){
+        if (array[i] % 2 !== 0){
             sum = sum + array[i];
         }
     } return sum;
